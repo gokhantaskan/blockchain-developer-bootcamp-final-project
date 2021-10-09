7b2def7c87c1433988bb60e629fbecc4db1e1ef7
Gökhan Taşkan Ubuntu <gokhan.taskan@blok-z.com>
2021-10-09 21:59:59 +0300

    init vue 3 for frontend

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "@vue/standard",
+    "@vue/typescript/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    "arrow-parens": [1, "as-needed"],
+    "comma-dangle": [1, { arrays: "always-multiline", objects: "always-multiline", imports: "never", exports: "never", functions: "never" }],
+    "max-len": [0, { code: 80, ignoreComments: false, ignoreStrings: false }],
+    "no-console": 1,
+    "no-debugger": 1,
+    "vue/html-indent": [1, 2, { attribute: 1, baseIndent: 1, closeBracket: 0, alignAttributesVertically: false, ignores: [] }],
+    indent: 1,
+    quotes: [1, "double", "avoid-escape"],
+    semi: [1, "always", { omitLastInOneLineBlock: true }],
+  },
+};
