d630e930faca7e5e7f8b4db5a23b2617cd986aa9
Gökhan Taşkan <gokhan.frontfeed@gmail.com>
2021-11-26 13:50:48 +0300

    add truffle visual debugger

--- blockchain/truffle-config.js
+++ blockchain/truffle-config.js
@@ -95,6 +95,10 @@ module.exports = {
     }
   },
 
+  plugins: [
+    "truffle-plugin-debugger"
+  ],
+
   // Truffle DB is currently disabled by default; to enable it, change enabled:
   // false to enabled: true. The default storage location can also be
   // overridden by specifying the adapter settings, as shown in the commented code below.
